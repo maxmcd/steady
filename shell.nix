a80540a99f9cc7c81082c96de6e0549146482183
maxmcd <max.t.mcdonnell@gmail.com>
2023-01-23 19:24:17 -0300

    try out nix for test env

---
+++ shell.nix
@@ -0,0 +1,10 @@
+with (import <nixpkgs> {});
+mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [
+        temporalite
+        bun
+        minio
+        go
+    ];
+}
