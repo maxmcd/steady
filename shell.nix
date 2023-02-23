3d8984cc9f1605eb4835f1c8a391fc63c92e2e37
maxmcd <max.t.mcdonnell@gmail.com>
2023-02-23 21:17:16 +0000

    pin nix version

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-with (import <nixpkgs> {});
+
+
+# https://github.com/NixOS/nixpkgs/commit/c95bf18beba4290af25c60cbaaceea1110d0f727
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c95bf18beba4290af25c60cbaaceea1110d0f727.tar.gz") {});
 mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
